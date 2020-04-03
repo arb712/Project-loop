2f617d464abdd340ce2de04cfb1b66fcc7c774c3
arb712 <ariqbimantoro@gmail.com>
2020-04-03 17:28:27 +0700

    add file

---
+++ loop.js
@@ -0,0 +1,51 @@
+//FOR FACTORIAL
+// let inputNum = 4;
+// let numOfFactorial = 1;
+// debugger;
+// for (i = 0; i < inputNUm; i++) {
+//     console.log((inputNUm - i));
+//     numOfFactorial = numOfFactorial * (inputNUm - i);
+    
+// }
+
+// console.log('Factorial' + inputNUm + ' is = ' + numOfFactorial);
+
+// REVERSE STRING
+// let reverseA = [
+//     `Q`,`I`,`R`,`A`
+// ]
+// let aRev = reverseA.reverse();
+
+// const ReverseString = string => [...string].reverse().join(''); 
+  
+// console.log(ReverseString("QIRA")); 
+
+// REVERSE ARRAY
+// let arrayR = [
+//     'INTEL','AMD,','NVIDIA'
+// ]
+// let arr = arrayR.reverse();
+// console.log(arr);
+
+// ASSIGN GRADE
+// for (let nilai = 80;nilai >= 0;nilai++){
+//     if(nilai === 50){
+//         console.log('Nilai kamu D')
+//         break;
+//     }else if(nilai === 70){
+//         console.log('Nilai kamu C')
+//         break;
+//     }else{
+//         'Nilai kamu good'
+//         break;
+//     }
+// }
+
+
+
+// MULTIPLIER IN FOR
+// let num = 3;
+
+// for (let nomor = 0; nomor < num; nomor++){
+//     console.log(`9 * ${nomor}`);
+// }
